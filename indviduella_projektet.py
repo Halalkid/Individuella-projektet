c213fe01156e2c1212d642f21b51f8453309aa64
lcarlstrom <lukaskurtcarlstrom@gmail.com>
2026-01-08 14:12:29 +0100

    Små fixes
    Lade till ett try except statement där användaren måste skriva en valid IP-address.  
    Lade till ett timeout statement för att bryta uppkoppling när koden inte får ett svar på 1 sekund (brukar betyda att porten är avstängd, innebär att koden körs fortare). 
    Rättade "open_port" till "open_ports". Koden fungerade inte innan p.g.a denna felstavning. 
    Lade till open_ports.sort() för att sortera listan så att portar printas i korrekt ordning.

--- indviduella_projektet.py
+++ indviduella_projektet.py
@@ -3,43 +3,53 @@
 import socket
 import threading
 from queue import Queue
+import ipaddress
+
+
 
 target = input("Ange IP-adress at skanna: ")
-queue = Queue()
-open_ports = []
 
-def portscan(port):
-    try:
-        s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s1.connect((target, port))
-        return True
-    except:
-        return False
+try:
+    ipaddress.ip_address(target)
+    queue = Queue()
+    open_ports = []
 
-def fill_queue(port_list):
-    for port in port_list:
-        queue.put(port)
+    def portscan(port):
+        try:
+            s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s1.settimeout(1)
+            s1.connect((target, port))
+            return True
+        except:
+            return False
 
-def worker():
-    while not queue.empty():
-        port = queue.get()
-        if portscan(port):
-                print("Port{} is open!".format(port))
-                open_port.append(port)
+    def fill_queue(port_list):
+        for port in port_list:
+            queue.put(port)
 
-port_list = range(1, 1024)
-fill_queue(port_list)
+    def worker():
+        while not queue.empty():
+            port = queue.get()
+            if portscan(port):
+                    print("Port{} is open!".format(port))
+                    open_ports.append(port)                        
+    port_list = range(1, 1024)
+    fill_queue(port_list)
+    open_ports.sort()
 
-thread_list = []
+    thread_list = []
 
-for i in range(100):
-    thread = threading.Thread(target=worker)
-    thread_list.append(thread)
+    for i in range(100):
+        thread = threading.Thread(target=worker)
+        thread_list.append(thread)
 
-for thread in thread_list:
-    thread.start()
+    for thread in thread_list:
+        thread.start()
 
-for thread in thread_list:
-    thread.join()
+    for thread in thread_list:
+        thread.join()
 
-print("Open ports are: ", open_ports)
+    print("Open ports are: ", open_ports)
+
+except ValueError:
+    print("Please only enter a valid IP-address")
